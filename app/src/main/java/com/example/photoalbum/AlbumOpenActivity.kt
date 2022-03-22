7b60503a8b5039d460fc97d2765ded15885393bf
hanchang97 <nimok97@gmail.com>
2022-03-23 01:04:35 +0900

    feat : 권한 응답에 따른 처리 구현 2번 거절 시 설정 화면으로 이동되게 구현

--- app/src/main/java/com/example/photoalbum/AlbumOpenActivity.kt
+++ app/src/main/java/com/example/photoalbum/AlbumOpenActivity.kt
@@ -2,13 +2,18 @@ package com.example.photoalbum
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.photoalbum.databinding.ActivityAlbumOpenBinding
 import com.google.android.material.snackbar.Snackbar
@@ -16,17 +21,16 @@ import com.google.android.material.snackbar.Snackbar
 class AlbumOpenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAlbumOpenBinding
+    private lateinit var getAlbumContent: ActivityResultLauncher<Intent>
+
+    private val REQUEST_CODE_READ_EXTERNAL_STORAGE = 1004
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_album_open)
 
-        setAlbumOpenBtn()
-    }
-
-    private fun setAlbumOpenBtn() {
-        val getContent =
+        getAlbumContent =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode == RESULT_OK) {
                     Log.d("AppTest", "RectangleActivity/ data : ${it.data?.data}")
@@ -41,7 +45,6 @@ class AlbumOpenActivity : AppCompatActivity() {
                         } else {
                             MediaStore.Images.Media.getBitmap(contentResolver, it.data!!.data)
                         }
-
                         //
 
                     } catch (e: Exception) {
@@ -52,21 +55,54 @@ class AlbumOpenActivity : AppCompatActivity() {
                 }
             }
 
-        val requestPermissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-                if (isGranted) {
+
+        setAlbumOpenBtn()
+    }
+
+    private fun setAlbumOpenBtn() {
+        binding.btnOpenAlbum.setOnClickListener {
+
+            val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_READ_EXTERNAL_STORAGE)
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_READ_EXTERNAL_STORAGE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("AppTest", "권한 승인")
+                    Snackbar.make(binding.root, "권한이 승인되었습니다", Snackbar.LENGTH_SHORT).show()
+
                     val intent = Intent(Intent.ACTION_PICK)
                     intent.type = "image/*"
-                    getContent.launch(Intent.createChooser(intent, "Gallery"))
+                    getAlbumContent.launch(Intent.createChooser(intent, "Gallery"))
                 } else {
-                    Snackbar.make(binding.root, "갤러리 접근 권한이 승인되지 않았습니다", Snackbar.LENGTH_SHORT)
-                        .show()
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                    ) {
+                        Log.d("AppTest", "권한 최초 거절")
+                        Snackbar.make(binding.root, "권한이 승인되지 않았습니다", Snackbar.LENGTH_SHORT).show()
+
+                    } else {
+                        Log.d("AppTest", "두 번째 거절")
+                        Snackbar.make(binding.root, "'설정'에서 직접 권한 승인이 필요합니다", Snackbar.LENGTH_SHORT).show()
+
+                        // '설정' 화면으로 이동하기
+                        val intent = Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", packageName, null))
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }
                 }
             }
-
-        binding.btnOpenAlbum.setOnClickListener {
-            // 갤러리 열고 uri 가져오기 구현하기
-            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
     }
 }
